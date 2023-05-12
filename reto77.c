e23e4c9e0b70c6e44b913c9494613f678f66ae84
14748 <99936563+14748@users.noreply.github.com>
2023-05-12 12:12:09 +0200

    Update reto77.c

--- reto77.c
+++ reto77.c
@@ -1,46 +1,44 @@
+#include <wiringPi.h>
 #include <wiringPiI2C.h>
 
-// Define I2C bus and LCD address
-#define I2C_ADDR 0x27
-#define LCD_CHR  1
-#define LCD_CMD  0
+#define LCD_ADDRESS 0x27
+#define LCD_WIDTH 16
 
-// Function to send commands to the LCD
-void lcd_command(int fd, unsigned char cmd) {
-  wiringPiI2CWriteReg8(fd, LCD_CMD, cmd);
-  delay(50);
+int lcd;
+
+void lcd_command(unsigned char command) {
+    wiringPiI2CWrite(lcd, command);
+    delay(5);
 }
 
-// Function to send data to the LCD
-void lcd_data(int fd, unsigned char data) {
-  wiringPiI2CWriteReg8(fd, LCD_CHR, data);
-  delay(50);
+void lcd_init() {
+    lcd_command(0x33);
+    lcd_command(0x32);
+    lcd_command(0x06);
+    lcd_command(0x0C);
+    lcd_command(0x28);
+    lcd_command(0x01);
+    delay(5);
 }
 
-// Function to initialize the LCD
-void lcd_init(int fd) {
-  lcd_command(fd, 0x33);
-  lcd_command(fd, 0x32);
-  lcd_command(fd, 0x06);
-  lcd_command(fd, 0x0C);
-  lcd_command(fd, 0x28);
-  lcd_command(fd, 0x01);
-  delay(50);
+void lcd_clear() {
+    lcd_command(0x01);
+    delay(5);
+}
+
+void lcd_write_string(char *str) {
+    while (*str) {
+        wiringPiI2CWriteReg8(lcd, 0x40, *str++);
+    }
 }
 
 int main() {
-  // Initialize the I2C bus and LCD display
-  int fd = wiringPiI2CSetup(I2C_ADDR);
-  lcd_init(fd);
+    wiringPiSetup();
+    lcd = wiringPiI2CSetup(LCD_ADDRESS);
 
-  // Set the cursor to the start of the display
-  lcd_command(fd, 0x80);
+    lcd_init();
+    lcd_clear();
+    lcd_write_string("Hello, world!");
 
-  // Write the string to the display
-  char* message = "Hello, world!";
-  for(int i = 0; i < strlen(message); i++) {
-    lcd_data(fd, message[i]);
-  }
-
-  return 0;
+    return 0;
 }
